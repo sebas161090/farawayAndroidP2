6cfd2940e9a30e5b7a68c17fb679d17bf304a909
jmendozarv91 <jmendozarv@outlook.com>
2020-04-01 23:05:38 -0500

    cambios

---
+++ app/src/main/java/pe/edu/upc/faraway/activity/LoginActivity.java
@@ -0,0 +1,104 @@
+package pe.edu.upc.faraway.activity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import pe.edu.upc.faraway.bean.response.LoginRes;
+import pe.edu.upc.faraway.util.Constants;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private ProgressDialog loading;
+    private SharedPreferences prefs;
+
+    //UI
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+
+        prefs = getSharedPreferences(Constants.PREFS, Context.MODE_PRIVATE);
+
+        getSupportActionBar().hide();
+
+        bindUI();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+                prepareLogin(username, password);
+            }
+        });
+    }
+
+    private void bindUI(){
+//        loading = Loading.init(this);
+//        editTextUsername = findViewById(R.id.editTxtUsername);
+//        editTextPassword = findViewById(R.id.editTxtPassword);
+//        btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    private void prepareLogin(String username, String password){
+        Log.i("login","inicio");
+        if(TextUtils.isEmpty(username)){
+            Toast.makeText(this, "Usuario no valido", Toast.LENGTH_LONG).show();
+        }else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Password no valido", Toast.LENGTH_LONG).show();
+        }else {
+            login(username, password);
+        }
+    }
+
+    private void login(String username, String password){
+//        LoginService loginService = Api.getApi().create(LoginService.class);
+//        Call<LoginRes> login = loginService.login(new LoginReq(username, password));
+//        Loading.show(loading);
+//        login.enqueue(new Callback<LoginRes>() {
+//            @Override
+//            public void onResponse(Call<LoginRes> call, Response<LoginRes> response) {
+//                Loading.close(loading);
+//                if(response.code()==200){
+//                    LoginRes loginRes = response.body();
+//                    saveToken(loginRes);
+//                }else{
+//                    Toast.makeText(LoginActivity.this, "Usuario y/o contraseña incorrectos", Toast.LENGTH_LONG).show();
+//                }
+//            }
+//            @Override
+//            public void onFailure(Call<LoginRes> call, Throwable t) {
+//                Loading.close(loading);
+//                Toast.makeText(LoginActivity.this, Constants.ERROR_GENERIC, Toast.LENGTH_LONG).show();
+//            }
+//        });
+    }
+
+    private void saveToken(LoginRes loginRes){
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putString(Constants.TOKEN, loginRes.getToken());
+//        editor.commit();
+//
+//        goToAcceptanceSearch();
+    }
+
+    private void goToAcceptanceSearch(){
+//        Intent intent = new Intent(this, AcceptanceSearchActivity.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        startActivity(intent);
+    }
+}
