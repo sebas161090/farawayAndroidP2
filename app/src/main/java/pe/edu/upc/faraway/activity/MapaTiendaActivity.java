eb48242d8fb86f3de24451a16d78d7186604265f
antonio.flores <pablo.antonio.flores@fisglobal.com>
2020-04-24 18:42:44 -0500

    Adding activities for shop map and product registration

---
+++ app/src/main/java/pe/edu/upc/faraway/activity/MapaTiendaActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.faraway.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import pe.edu.upc.faraway.R;
+
+import android.os.Bundle;
+
+public class MapaTiendaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa_tienda);
+    }
+}
---
+++ app/src/main/java/pe/edu/upc/faraway/activity/RegistroProductoActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.faraway.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import pe.edu.upc.faraway.R;
+
+import android.os.Bundle;
+
+public class RegistroProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_producto);
+    }
+}
